435363fc994a499b449f299a355d364139bb1beb
Benjamin <s.benjamin@live.fr>
2018-01-30 13:46:41 +0100

    Boom bitch

--- main_test.c
+++ main_test.c
@@ -16,13 +16,17 @@ int main()
 	void *(*my_malloc)(size_t);
 	char *error;
 
-	handle = dlopen("./libmalloc_bootstrap.so", RTLD_LAZY);
-	if (!handle)
+	handle = dlopen("/home/benjamin/Dropbox/PSU_2017_malloc/cmake-build-debug/libmy_malloc.so", RTLD_LAZY);
+	if (!handle){
+		printf("lol\n");
 		return (84);
+	}
 	dlerror();
-	(void *)(&my_malloc) = dlsym(handle, "my_malloc");
-	if ((error = dlerror()) != NULL)
+	*(void **)(&my_malloc) = dlsym(handle, "my_malloc");
+	if ((error = dlerror()) != NULL){
+		printf("Non\n");
 		return (84);
+	}
 	dlclose(handle);
 	return (0);
 }
