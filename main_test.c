5b0b20a2b1ed6440255b53f05f577e34da2bd527
Benjamin <s.benjamin@live.fr>
2018-01-30 13:57:42 +0100

    Auto stash before merge of "master" and "origin/master"

--- main_test.c
+++ main_test.c
@@ -15,8 +15,12 @@ int main()
 	void *handle;
 	void *(*my_malloc)(size_t);
 	char *error;
+	char *errstr;
 
 	handle = dlopen("./libmy_malloc.so", RTLD_LAZY);
+	if (errstr != NULL)
+		printf ("A dynamic linking error occurred: (%s)\n", errstr);
+	errstr = dlerror();
 	if (!handle){
 		printf("cannot open lib\n");
 		return (84);
